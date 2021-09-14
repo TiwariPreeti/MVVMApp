aba0ff6ed56807a1ad79234150b36dbe451c647c
Preeti Tiwari <33624858+TiwariPreeti@users.noreply.github.com>
2021-09-14 11:11:54 +0530

    Add files via upload

---
+++ MVVM_Swift-master/MVVM_New/TableViewCells/EmployeeTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  EmployeeTableViewCell.swift
+//  MVVM_New
+//
+//  Created by Preeti Tiwari on 20/05/20.
+//  Copyright © 2020 Preeti Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var employeeIdLabel: UILabel!
+    @IBOutlet weak var employeeNameLabel: UILabel!
+    
+    var employee : EmployeeData? {
+        didSet {
+            employeeIdLabel.text = employee?.id
+            employeeNameLabel.text = employee?.employeeName
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MVVM_Swift-master/MVVM_New/ViewModels/EmployeesViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  EmployeesViewModel.swift
+//  MVVM_New
+//
+//  Created by Preeti Tiwari on 20/05/20.
+//  Copyright © 2020 Preeti Tiwari. All rights reserved.
+//
+
+import Foundation
+
+class EmployeesViewModel : NSObject {
+    
+    private var apiService : APIService!
+    private(set) var empData : Employees! {
+        didSet {
+            self.bindEmployeeViewModelToController()
+        }
+    }
+    
+    var bindEmployeeViewModelToController : (() -> ()) = {}
+    
+    override init() {
+        super.init()
+        self.apiService =  APIService()
+        callFuncToGetEmpData()
+    }
+    
+    func callFuncToGetEmpData() {
+        self.apiService.apiToGetEmployeeData { (empData) in
+            self.empData = empData
+        }
+    }
+}
